1474f6042830b200a30deb829a47e9885582a4c1
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2022-03-07 23:22:28 +0100

    insertion produit modèle mvc

---
+++ libraries/Models/Produits.php
@@ -0,0 +1,55 @@
+<?php
+
+    namespace Models; 
+
+    require_once('Model.php');
+    
+    class Produits extends Model{
+
+        public $id;
+        public $nom;
+        public $reference;
+        public $classe;
+        public $description;
+        public $id_categorie;
+        public $id_sous_categorie;
+        public $prix;
+        public $image;
+        protected $table = 'categories';
+        protected $table_deux = 'sous_categories';
+        protected $table_par_id = 'produits';
+        protected $table_verif = 'produits';
+
+
+        public function inserer_produit($nom, $reference, $classe, $description, $id_categorie, $id_sous_categorie, $prix, $image){
+
+            try {
+                // $id_categorie = intval($id_categorie);
+                $insertion = "INSERT INTO produits (nom, reference, classe, description, id_categorie, id_sous_categorie, prix, image1) VALUES (:nom, :reference, :classe, :description, :id_categorie, :id_sous_categorie, :prix, :image)";
+                $result = $this->bdd->prepare($insertion);
+                $result->bindValue(':nom', $nom, \PDO::PARAM_STR);
+                $result->bindValue(':reference', $reference, \PDO::PARAM_STR);
+                $result->bindValue(':classe', $classe, \PDO::PARAM_STR);
+                $result->bindValue(':description', $description, \PDO::PARAM_STR);
+                $result->bindValue(':id_categorie', $id_categorie, \PDO::PARAM_INT);
+                $result->bindValue(':id_sous_categorie', $id_sous_categorie, \PDO::PARAM_INT);
+                $result->bindValue(':prix', $prix, \PDO::PARAM_STR);
+                $result->bindValue(':image', $image, \PDO::PARAM_STR);
+                $result->execute();
+            }
+            catch( PDOException $Exception ) {
+                // Note The Typecast To An Integer!
+                var_dump($Exception);
+            }
+        }
+
+
+        public function selection_produits(){
+            $selection = "SELECT * FROM produits";
+            $result = $this->bdd->prepare($selection);
+            $result->execute();
+            $fetch3 = $result->fetchAll();
+            return $fetch3;
+        }
+    }
+?>
---
+++ libraries/Views/Produit.php
@@ -0,0 +1,76 @@
+<?php
+
+    require_once('../Models/Produits.php');
+    require_once('../Models/Model.php');
+    require_once('../Controllers/Produits.php');
+
+    $produits = new Models\Model();
+    $fetchCategories = $produits->recuperation_de_donnee();
+    $fetchSousCategories = $produits->recuperation_de_donnee2();
+
+    if(!empty($_POST['nom']) && !empty($_POST['reference']) && !empty($_POST['classe']) && !empty($_POST['description']) && !empty($_POST['categorie']) && !empty($_POST['sous-categorie']) && !empty($_POST['prix']) && !empty($_POST['image'])){
+        $produit = new Controllers\Produits();
+        $produit->creerProduit($_POST['nom'], $_POST['reference'], $_POST['classe'], $_POST['description'], $_POST['categorie'], $_POST['sous-categorie'], $_POST['prix'], $_POST['image']);
+    }
+    else if(isset($nom) || isset($reference) || isset($_POST["classe"]) || isset($description) || isset($_POST["id_utilisateur"]) || isset($_POST["categorie"]) || isset($_POST["sous-categorie"]) || isset($prix) || isset($image)){
+        echo 'champ vide';
+    }
+?>
+
+
+<html>
+    
+    <main>
+
+        <form action="" method="post">
+            <label for="nom">nom</label>
+            <input type="text"  name="nom">
+
+            <label for="reference">reference</label>
+            <input  name="reference">
+
+            <label for="classe">classe</label>
+            <select name="classe">
+                <option >Choisissez une option</option>
+                <option value="homme">Homme</option>
+                <option value="femme">Femme</option>
+                <option value="enfant">Enfant</option>
+                <option value="Sport">Sport</option>
+            </select>
+
+            <label for="description">description</label>
+            <input type="text" name="description">
+
+            <select name="categorie">
+                <option value="choose" name="choose">Choisir une catégorie d'article</option>
+                <option>
+                    <?php
+                        foreach($fetchCategories as $value) {
+                            echo "<option value=".$value["id"].">" .$value["nom"]. "</option>";
+                        }
+                    ?>
+                </option>
+            </select>
+
+            <select name="sous-categorie">
+                <option value="choose" name="choose">Choisir une sous-catégorie d'article</option>
+                <option>
+                    <?php
+                        foreach($fetchSousCategories as $value) {
+                            echo "<option value=".$value["id"].">" .$value["nom"]. "</option>";
+                        }
+                    ?>
+                </option>
+            </select>
+
+    <label for="image">image</label>
+    <input type="text" name="image">
+
+    <label for="prix">prix</label>
+    <input type="text" name="prix">
+
+    <input type="submit" value="creer">
+</form>
+
+</main>
+</html
