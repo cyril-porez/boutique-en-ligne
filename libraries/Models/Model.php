ff70d678ef0f60056d4414e9a6a6390c96fcd3de
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2022-03-08 02:02:56 +0100

    restructuration du code architecture MVC

--- libraries/Models/Model.php
+++ libraries/Models/Model.php
@@ -5,15 +5,14 @@
     class Model{
 
         protected $id;
-        protected $table;
-        protected $table_deux;
-        protected $table_par_id;
-        protected $table_verif;
-
+        protected $table = 'categories';
+        protected $table_deux = 'sous_categories';
+        protected $table_par_id = 'produits';
+        protected $table_verif = 'produits';
 
         public function __construct(){
-            $bdd = new PDO("mysql:host=localhost;dbname=carnage",'root','');
-            $bdd->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+            $bdd = new \PDO("mysql:host=localhost;dbname=carnage",'root','');
+            $bdd->setAttribute(\PDO::ATTR_ERRMODE , \PDO::ERRMODE_EXCEPTION);
             $this->bdd = $bdd;
             return $bdd;
         }
@@ -38,7 +37,7 @@
         public function recuperation_par_id($id){
             $selection = "SELECT * FROM {$this->table_par_id} WHERE id = :id";
             $result = $this->bdd->prepare($selection);
-            $result->bindValue(':id', $id, PDO::PARAM_INT);
+            $result->bindValue(':id', $id, \PDO::PARAM_INT);
             $result->execute();
             $recuperer_tout = $result->fetchAll();
             return $recuperer_tout;
@@ -47,7 +46,7 @@
         public function verif_si_existe_deja($nom){
             $selection = "SELECT nom FROM {$this->table_verif} WHERE nom = :nom";
             $result = $this->bdd->prepare($selection);
-            $result->bindValue(':nom', $nom, PDO::PARAM_STR);
+            $result->bindValue(':nom', $nom, \PDO::PARAM_STR);
             $result->execute();
             $recupere = $result->fetchAll();
             return $recupere;
