2522d37181097803b530c72cdbac0e6decc32655
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2022-03-10 11:08:46 +0100

    envoie du mail avec phpmailer vérifie que l'email existe en base de donnée

---
+++ blabla/motdepasseoublie.php
@@ -0,0 +1,94 @@
+<?php
+
+    use PHPMAILER\PHPMAILER\PHPMAILER;
+    use PHPMAILER\PHPMAILER\EXCEPTION;
+    use PHPMailer\PHPMailer\SMTP;
+
+    require_once "vendor/phpmailer/phpmailer/src/Exception.php";
+    require_once "vendor/phpmailer/phpmailer/src/PHPMailer.php";
+    require_once "vendor/phpmailer/phpmailer/src/SMTP.php";
+
+    //require 'PHPMailerAutoload.php';
+    require 'vendor/autoload.php';
+    $mail = new PHPMAILER();
+
+    
+        $bdd = new PDO("mysql:host=localhost;dbname=carnage", "root", "");
+        // set the PDO error mode to exception
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+    if (!empty($_POST['recupEmail'])) {
+        
+        $email = $_POST['recupEmail'];
+        $recupEmail = $bdd->prepare("SELECT email from utilisateurs WHERE email = '$email'");
+        $recupEmail->execute();
+        $existe = $recupEmail->rowcount();
+
+        if ($existe > 0) {
+            
+            try {
+    
+                //Configuration
+                //$mail->SMTPDebug = SMTP::DEBUG_SERVER; // information de debug
+        
+                //Configuration SMTP
+                $mail->isSMTP();
+                $mail->Host = "smtp.gmail.com";
+                $mail->SMTPAuth = "true";
+                $mail->Username = 'cyrilporez@gmail.com';   //SMTP username
+                $mail->Password = 'Hokuto1989@';            //SMTP mot de passe
+                $mail->SMTPSecure = 'tls';
+                $mail->Port = 587;
+        
+                //Charset
+                $mail->CharSet = "utf-8"; 
+        
+                //destinataire
+                $mail->addAddress($email);
+        
+                //Epéditeur 
+                $mail->setFrom('cyrilporez@gmail.com');
+            
+                $mail->Subject = 'Mot de passe oublié';
+                $mail->Body = "http://localhost/boutique-en-ligne/blabla/nouveauMotdepasse.php";
+        
+                //envoie du mail
+                $mail->send();
+                echo "Le mail a bien été envoyé. Veuillez vérifier votre boîte mail afin de changer votre mot de passe !";
+            }
+            catch(Exception) {
+                echo "Message non envoyé error: {$mail->ErrorInfo}"; 
+            }
+        }
+        else {
+            echo "Ce mail nfdjk,";
+        }
+    }
+    else if (isset($_POST["recupEmail"])) {
+        echo "le champ est vide !";
+    } 
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mot de passe oublié</title>
+</head>
+<body>
+    <main>
+        <h1>MOT DE PASSE OUBLIE</h1>
+        <h3>Veuillez entrer votre adresse email ci-dessous pour recevoir un lien de réinitialisation de mot de passe.</h3>
+
+        <form action="" method="post">
+            <input type="email" name="recupEmail">
+            <input type="submit" name="recup" value="Réinitialier mon mot de passe">
+            <input type="submit" name="retour" value="Retour">
+        </form>
+    </main>
+</body>
+</html>
