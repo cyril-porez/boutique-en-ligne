1f365b58e9dededca30d0110bea3da6af945c756
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2022-04-07 14:55:09 +0200

    affichage des images prix et nom du produit

--- libraries/Views/produits.php
+++ libraries/Views/produits.php
@@ -1,4 +1,5 @@
 <?php
+    //require_once('header.php');
     require_once('../Models/Produits.php');
     require_once('../Models/SousCategorie.php');
     require_once('../Models/Categorie.php');
@@ -6,14 +7,32 @@
     $id = $_GET['id'];
     
     $sous_categorie = new \Models\SousCategorie;
-    $affiche_sous_categorie = $sous_categorie->choix_produit_sous_categorie($id);
-    var_dump($affiche_sous_categorie);
+    $affiche_sous_categories = $sous_categorie->choix_produit_sous_categorie($id);
     
-    echo $id;
+    
+
+    var_dump($affiche_sous_categories);
+    
+   // echo $id;
 ?>
 
 <html>
+    <body>
+        <header>
 
-    
-    
+        </header>
+        <main>
+            <?php
+                foreach($affiche_sous_categories as $affiche_sous_categories) {?>
+                    <img src=<?= $affiche_sous_categories['image1']; ?> alt="image carnage"> <?php
+                    echo '<br>' . $affiche_sous_categories['nom']. '<br>';
+                    echo $affiche_sous_categories['prix'] . " €" . '<br>';?>
+                    <button>Voir Produit</button><br><?php
+                }
+                ?>
+        </main>
+        <footer>
+
+        </footer>
+    </body>   
 </html>
