c83c952ff57efd70dbc5f458463c3c4ce86b4a69
Sylla Ibrahim <ibrahim-sylla>
2022-04-12 12:47:46 +0200

    style page mot de passe oublie

--- libraries/motdepasseoublie.php
+++ libraries/motdepasseoublie.php
@@ -13,11 +13,11 @@
     $mail = new PHPMAILER();
 
     $date = date('Y-m-d H:i:s');
-    
+
         $bdd = new PDO("mysql:host=localhost;dbname=carnage", "root", "");
         // set the PDO error mode to exception
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
+
     if (!empty($_POST['recupEmail'])) {
         $token = uniqid();
         $url = "http://localhost/boutique-en-ligne/libraries/nouveauMotdepasse?token=$token";
@@ -27,16 +27,16 @@
         $existe = $recupEmail->rowcount();
 
         if ($existe > 0) {
-            
+
             try {
 
                 $modifToken = $bdd->prepare("UPDATE utilisateurs SET token = '$token', date = '$date' WHERE email = '$email'");
                 $modifToken->execute();
 
-    
+
                 //Configuration
                 //$mail->SMTPDebug = SMTP::DEBUG_SERVER; // information de debug
-        
+
                 //Configuration SMTP
                 $mail->isSMTP();
                 $mail->Host = "smtp.gmail.com";
@@ -45,25 +45,25 @@
                 $mail->Password = 'Hokuto1989@';            //SMTP mot de passe
                 $mail->SMTPSecure = 'tls';
                 $mail->Port = 587;
-        
+
                 //Charset
-                $mail->CharSet = "utf-8"; 
-        
+                $mail->CharSet = "utf-8";
+
                 //destinataire
                 $mail->addAddress($email);
-        
-                //Epéditeur 
+
+                //Epéditeur
                 $mail->setFrom('cyrilporez@gmail.com');
-            
+
                 $mail->Subject = 'Mot de passe oublié';
                 $mail->Body = "Bonjour, Voici votre lien pour la réinitialisation du mot de passe : $url";
-        
+
                 //envoie du mail
                 $mail->send();
                 echo "Le mail a bien été envoyé. Veuillez vérifier votre boîte mail afin de changer votre mot de passe !";
             }
-            catch(Exception) {
-                echo "Message non envoyé error: {$mail->ErrorInfo}"; 
+            catch(Exception $e) {
+                echo "Message non envoyé error: {$mail->ErrorInfo}";
             }
         }
         else {
@@ -83,6 +83,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/motdepasseoublié.css">
     <title>Mot de passe oublié</title>
 </head>
 <body>
--- libraries/nouveauMotdepasse.php
+++ libraries/nouveauMotdepasse.php
@@ -40,6 +40,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/motdepasseoublié.css">
+
     <title>Nouveau Mot de passe</title>
 </head>
 <body>
@@ -47,7 +49,6 @@
         <h1>DEFINIR MOT DE PASSE</h1>
 
         <form action="" method="post">
-            
             <label for="nouveauMotdepasse">Nouveau mot de passe</label>
             <input type="password" name="nouveauMotdepasse">
 
