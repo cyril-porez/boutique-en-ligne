c1b887430f32f11f842b4950b23c4df0393d4681
Sylla Ibrahim <ibrahim-sylla>
2022-04-15 14:12:37 +0200

    style admin.php

--- libraries/Views/admin.php
+++ libraries/Views/admin.php
@@ -3,22 +3,42 @@
 ?>
 
 <body>
+    <link rel="stylesheet" href="css/admin.css">
     <header>
 
     </header>
     <main>
-        
-            <button><a href="gestionUtilisateurs.php">Gestion des Utilisateurs</a></button>
-        
-        
-            <button><a href="gestionCategories.php">Gestion de Catégories</button>
-        
-        
-            <button><a href="gestionProduits.php">Gestion des Produits</a></button>
-      
-      
-            <button><a href="gestionCommentaires.php">Gestion des Commentaires</a></button>
-        
+        <img src="images/admin.png" alt="">
+        <div class="container">
+            <button>
+                <a href="gestionUtilisateurs.php">
+                    Gestion des Utilisateurs<br>
+                    Créer, supprimer ou<br>modifier un utilisateurs
+                </a>
+            </button>
+
+            <button>
+                <a href="gestionCategories.php">
+                    Gestion de Catégories <br>
+                    Créer, supprimer ou <br> modifier une catégories
+                </a>
+            </button>
+        </div>
+        <div class="container">
+            <button>
+                <a href="gestionProduits.php">
+                    Gestion des Produits <br>
+                    Créer, supprimer ou <br> modifier les produits
+                </a>
+            </button>
+
+            <button>
+                <a href="gestionCommentaires.php">
+                    Gestion des Commentaires <br>
+                    Créer, supprimer ou <br> modifier un commentaires
+                </a>
+            </button>
+        </div>
     </main>
     <footer>
 
