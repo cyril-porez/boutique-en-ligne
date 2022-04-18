7c71f2097239a8ab3eb0d9f3f7e982974ca13a43
Sylla Ibrahim <ibrahim-sylla>
2022-04-18 14:49:18 +0200

    style panel admin

--- libraries/Views/admin.php
+++ libraries/Views/admin.php
@@ -11,23 +11,23 @@
         <img src="images/admin.png" alt="">
         <div class="container">
                 <a href="gestionUtilisateurs.php">
-                    <img src="images/utilisateurs.png" alt="" class="icone">
+                    <img class="icon" src="images/utilisateurs.png" alt="" class="icone">
                     Gestion des Utilisateurs
                 </a>
 
                 <a href="gestionCategories.php">
-                    <img src="images/categorie.png" alt="" class="icone">
+                    <img class="icon" src="images/categorie.png" alt="" class="icone">
                     Gestion de Catégories
                 </a>
         </div>
         <div class="container">
                 <a href="gestionProduits.php">
-                    <img src="images/produits.png" alt="" class="icone">
+                    <img class="icon" src="images/produits.png" alt="" class="icone">
                     Gestion des Produits
                 </a>
 
                 <a href="gestionCommentaires.php">
-                    <img src="images/commentaires.png" alt="" class="icone">
+                    <img class="icon" src="images/commentaires.png" alt="" class="icone">
                     Gestion des Commentaires
                 </a>
         </div>
