c908ef095982af6fe8836d7bef8fb8e3263b8021
Sylla Ibrahim <ibrahim-sylla>
2022-04-21 17:52:20 +0200

    debut index

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <section class="titre">
+            <h2>BIENVENUE</h2>
+        </section>
+        <section class="img-presentation">
+            <img src="images/entrer.webp" alt="">
+        </section>
+
+        <section class="titre">
+            <h2>CARNAGE / GOD OF WAR</h2>
+        </section>
+
+    <section class="img-presentation">
+        <img src="images/legendaire.webp" alt="">
+    </section>
+
+    <section class="titre">
+        <h2>CARNAGE / ENTRE DANS L'ARENE</h2>
+    </section>
+
+    <section class="img-presentaion">
+    <div class="parent">
+        <div class="div1"><img src="images/gant.jpg" alt=""></div>
+        <div class="div2"> </div>
+        <div class="div3"><img src="images/atlete.jpg" alt=""></div>
+    </div>
+    </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
