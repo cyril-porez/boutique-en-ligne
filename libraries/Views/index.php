525d57cb70e4ed8af98f102a7263e5f4df64197c
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2022-04-28 13:31:52 +0200

    modif index

--- libraries/Views/index.php
+++ libraries/Views/index.php
@@ -1,12 +1,12 @@
 <?php session_start();
-var_dump($_SESSION) ?>
+//var_dump($_SESSION) ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="css/index.css"> -->
+    <link rel="stylesheet" href="css/index.css"> 
     <title>Document</title>
 </head>
 <body>
@@ -18,7 +18,7 @@ var_dump($_SESSION) ?>
             <h2>BIENVENUE</h2>
         </section>
         <section class="img-presentation">
-            <img src="images/entrer.webp" alt="">
+            <img src="../images/entrer.webp" alt="">
         </section>
 
         <section class="titre">
@@ -26,7 +26,7 @@ var_dump($_SESSION) ?>
         </section>
 
         <section class="img-presentation">
-            <img src="images/legendaire.webp" alt="">
+            <img src="../images/legendaire.webp" alt="">
         </section>
 
         <section class="titre">
@@ -35,9 +35,9 @@ var_dump($_SESSION) ?>
 
         <section class="img-grid">
             <div class="parent">
-                <div class="div1"><img src="images/gant.jpg" alt=""></div>
+                <div class="div1"><img src="../images/gant.jpg" alt=""></div>
                 <!-- <div class="div2"></div> -->
-                <div class="div3"><img src="images/atlete.jpg" alt=""></div>
+                <div class="div3"><img src="../images/atlete.jpg" alt=""></div>
             </div>
         </section>
     </main>
